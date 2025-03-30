d54ef7a07a8c2e123240b7e9f91c9a5b4b2c3210
VANSIEM190 <vansiemgracia03@gmail.com>
2025-03-30 18:23:57 +0100

    my app TodoApp

--- src/services/firebaseConfig.js
+++ src/services/firebaseConfig.js
@@ -1,6 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAuth, signInWithEmailAndPassword  } from 'firebase/auth';
+import { getAuth  } from 'firebase/auth';
 import { getFirestore } from "firebase/firestore";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -26,4 +26,4 @@ const db = getFirestore(app);
 
 
 // Exporter l'authentification et les méthodes d'authentification
-export { auth, signInWithEmailAndPassword ,  db };
+export { auth,  db };
